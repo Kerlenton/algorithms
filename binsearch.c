8a4dc2a8fd3e991b196d7cdf990ecce17b1e7566
Semyon <51188729+Smekbody@users.noreply.github.com>
2019-07-08 20:11:54 +0400

    Update binsearch.c

--- binsearch.c
+++ binsearch.c
@@ -23,26 +23,3 @@ int binsearch(int x, int v[], int n)
 
 	return -1;
 }
-
-main()
-{
-	int v[100], x, n, i;
-
-	printf("Enter number of elements -> ");
-	scanf_s("%d", &n);
-
-	printf("Enter %d variables:\n", n);
-
-	for (i = 0; i < n; i++)
-	{
-		scanf_s("%d", &v[i]);
-	}
-
-	printf("Enter value to find -> ");
-	scanf_s("%d", &x);
-
-	printf("Value %d is located at index of %d\n", x, binsearch(x, v, n));
-
-	system("PAUSE");
-	return 0;
-}
