b66a967043f28685b91fb42ccf58d24cd0be6aac
Semyon <51188729+Kerlenton@users.noreply.github.com>
2020-01-10 20:25:21 +0400

    Update euclidean_algorithm.c

--- euclidean_algorithm.c
+++ euclidean_algorithm.c
@@ -1,6 +1,33 @@
 #include <stdlib.h>
 #include <assert.h>
 
+/*                                        lemma: GCD(a, b) = GCD(a-bq, b),     a, b - positive integers
+                                                                               q - integer
+									       
+									       
+     PROOF:  1) Let d = GCD(a, b), then a = dx, b = dy, then
+                     
+	        a - bq = dx - dyq = d(x-yq), b = dy, 
+	        therefore d|a, d|b (this means that d divides a, d divides b, i.e. d is a common divisor),
+		therefore GCD(a, b) = d <= GCD(a-bq, b).
+	     ---------------------------------------------------------------------------------------------
+	     2) Let t = GCD(a-bq, b), then a-bq = tz, b = tw, then
+	     
+	        a = tz + bq = tz + twq = t(z+wq)
+		therefore t|a, t|b,
+		therefore GCD(a-bq, b) = t <= GCD(a, b)
+	     ---------------------------------------------------------------------------------------------
+	     3) On the one hand:          GCD(a, b) = d <= GCD(a-bq, b).
+	        On the other hand:        GCD(a-bq, b) = t <= GCD(a, b).
+		
+		GCD(a, b) <= GCD(a-bq, b)   and    GCD(a, b) >= GCD(a-bq, b)
+		It means, that GCD(a, b) = GCD(a-bq, b)
+		                                                                      Q.E.D.
+                                                                                         
+    
+*/
+
+
 size_t GCD(size_t a, size_t b)
 {
 	assert(a >= 0);
