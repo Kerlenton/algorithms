6139ab15439171e1ad831a38e6b963349db3c410
Semyon <51188729+Kerlenton@users.noreply.github.com>
2020-05-03 18:19:21 +0400

    Create greedyal.c

---
+++ greedyal.c
@@ -0,0 +1,47 @@
+#include <stdint.h>
+#include <stdio.h>
+
+size_t factorial(size_t x)
+{
+	if (x == 0) return 1;
+	return x * factorial(x - 1);
+}
+
+signed int search(signed int n)
+{
+	signed int x = 1;
+	signed int i = x;
+
+	while (n - x >= 0)
+	{
+		i++;
+		x = factorial(i);
+		
+	}
+
+	return --i;
+}
+
+void greedyALG(signed int n)
+{
+	size_t j;
+
+	while (n != 0)
+	{
+		j = search(n);
+		printf("%u+", factorial(j));
+		n = n - factorial(j);
+	}
+
+	putchar('\b');
+	putchar(' ');
+	putchar('\n');
+}
+
+main()
+{
+	greedyALG(5);
+
+	system("PAUSE");
+	return 0;
+}
