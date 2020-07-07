80299411f1b59cea5beaa04a99ab9be5aafd5d58
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-07-07 09:16:52 +0400

    Add files via upload

---
+++ arrangement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+extern void arrangement(const char *const str, const unsigned char size)
+{
+	const unsigned char length = strlen(str);;
+	int x, y;
+
+	for (x = 0; x < pow(length, size); x++)
+	{
+		for (y = size - 1; y >= 0; y--)
+			putchar(str[(char)(x / pow(length, y)) % length]);
+		putchar('\n');
+	}
+}
