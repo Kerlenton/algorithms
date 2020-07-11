087340b7450328d98b63301c8e254b76b4c8c59a
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-07-11 16:12:00 +0400

    Add files via upload

---
+++ trigonometry/trig.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "head.h"
+
+static int factorial(int x)
+{
+	if (x == 0) return 1;
+	return x * (x - 1);
+}
+
+static double power(double  x, unsigned int y)
+{
+	if (y == 0) return 1;
+	return x * power(x, y - 1);
+}
+
+extern double cos(double x)
+{
+	double cos = 0;
+	signed int j = 1;
+	for (int i = 0; i < 100; i += 2)
+	{
+		if (j == 1)
+			cos += power(x, i) / factorial(i);
+		else
+			cos -= power(x, i) / factorial(i);
+
+		j *= -1;
+	}
+
+	return cos;
+}
+
+extern double sin(double x)
+{
+	double sin = x;
+	signed int j = 1;
+	for (int i = 3; i < 100; i += 2)
+	{
+		if (j == -1)
+			sin += power(x, i) / factorial(i);
+		else
+			sin -= power(x, i) / factorial(i);
+
+		j *= -1;
+	}
+
+	return sin;
+}
---
+++ trigonometry/trig.h
@@ -0,0 +1,9 @@
+#ifndef TRIG_H
+
+#define TRIG_H
+#define PI 3.14159265358979323846
+
+extern double cos(double x);
+extern double sin(double x);
+
+#endif
